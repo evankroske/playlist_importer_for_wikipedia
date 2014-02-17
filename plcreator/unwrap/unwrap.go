f4dbfdc864de21ce43a0da018f47e0b7cb288926
Evan Kroske <evan@evankroske.com>
2014-02-17 11:29:34 -0800

    Improved JSON query language.

--- plcreator/unwrap/unwrap.go
+++ plcreator/unwrap/unwrap.go
@@ -1,5 +1,12 @@
 package unwrap
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"text/scanner"
+)
+
 type UnwrapError struct {
 	msg string
 }
@@ -8,6 +15,46 @@ func (e *UnwrapError) Error() string {
 	return e.msg
 }
 
-func Unwrap(t interface{}, q string) (interface{}, error) {
-	return "unwrapped!", nil
+func Unwrap(tree interface{}, q string) (interface{}, error) {
+	s := new(scanner.Scanner)
+	s.Init(strings.NewReader(q))
+	t := s.Scan()
+	for t != scanner.EOF {
+		switch t {
+		case '.':
+			t = s.Scan()
+			if t != scanner.Ident {
+				return nil, &UnwrapError{
+					fmt.Sprintf(
+						"unwrap: Parse error: expected id, got %v",
+						scanner.TokenString(t),
+					),
+				}
+			}
+			m, ok := tree.(map[string]interface{})
+			if !ok {
+				return nil, &UnwrapError{"unwrap: Type error"}
+			}
+			if tree == nil {
+				return nil, &UnwrapError{"unwrap: Nil error"}
+			}
+			tree = m[s.TokenText()]
+		case '[':
+			t = s.Scan()
+			i, _ := strconv.Atoi(s.TokenText())
+			t = s.Scan()
+			if t != ']' {
+				return nil, &UnwrapError{
+					fmt.Sprintf(
+						"unwrap: Parse error: expected ']', got %v",
+						scanner.TokenString(t),
+					),
+				}
+			}
+			a := tree.([]interface{})
+			tree = a[i]
+		}
+		t = s.Scan()
+	}
+	return tree, nil
 }
--- plcreator/unwrap/unwrap_test.go
+++ plcreator/unwrap/unwrap_test.go
@@ -3,13 +3,47 @@ package unwrap
 import "testing"
 
 func TestUnwrapMap(t *testing.T) {
-	var obj interface{} = map[string]int{ "foo": 1 }
-	res, err := Unwrap(obj, ".foo")
+	var obj interface{} = map[string]interface{}{
+		"foo": map[string]interface{}{ "bar" : "baz" },
+	}
+	res, err := Unwrap(obj, ".foo.bar")
 	if err != nil {
 		t.Fatal(err)
 	}
-	v, _ := res.(int)
-	if v != 1 {
-		t.Fatal("Wrong type")
+	str, ok := res.(string)
+	if !ok {
+		t.Fatal("Wrong result type")
+	} else if str != "baz" {
+		t.Fatal("Wrong result value")
+	}
+}
+
+func TestUnwrapMapTypeError(t *testing.T) {
+	var array interface{} = make([]interface{}, 1)
+	_, err := Unwrap(array, ".foo")
+	if err == nil {
+		t.Fatal("Expected type error")
+	}
+
+}
+
+func TestUnwrapMapParseError(t *testing.T) {
+	var object interface{}
+	_, err := Unwrap(object, "..}")
+	if err == nil {
+		t.Fatal("Expected parse error")
+	}
+}
+
+func TestUnwrapArrayTypeError(t *testing.T) {
+	var array interface{} = []interface{}{ []interface{}{ 1 } }
+	res, err := Unwrap(array, "[0][0]")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v, ok := res.(int); !ok {
+		t.Fatal("Wrong result type")
+	} else if v != 1 {
+		t.Fatal("Wrong result value")
 	}
 }
