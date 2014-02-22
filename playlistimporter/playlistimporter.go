4dc9bbea351d7e448fc3cd1a960f5e9a91013560
Evan Kroske <evan@evankroske.com>
2014-02-22 13:11:29 -0800

    Split authentication handler into its own file, added licensing information, and moved admin tasks into a namespace.

--- playlistimporter/playlistimporter.go
+++ playlistimporter/playlistimporter.go
@@ -1,8 +1,24 @@
+/*
+Copyright 2014 Google Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package playlistimporter
 
 import (
 	"encoding/json"
     "fmt"
+	"html/template"
 	"io"
     "net/http"
 
@@ -12,10 +28,16 @@ import (
 	"playlistimporter/unwrap"
 )
 
+var loginPageTemplate *template.Template
+
 func init() {
     http.HandleFunc("/", handler)
-    http.HandleFunc("/refreshgenrelist", refreshGenreListHandler)
-	http.HandleFunc("/kickoffgenrediscovery", kickoffGenreDiscoveryHandler)
+    http.HandleFunc("/admin/discoverplaylists", refreshGenreListHandler)
+	http.HandleFunc(
+		"/admin/startplaylistdiscovery",
+		kickoffGenreDiscoveryHandler,
+	)
+	http.HandleFunc("/admin/login", loginHandler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
